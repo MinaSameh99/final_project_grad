93c5335be2375b14246a34e2d66924054d1707d8
MinaSameh99 <162881373+MinaSameh99@users.noreply.github.com>
2026-05-01 02:29:58 +0300

    Add files via upload

---
+++ univesity_system_final.sql
@@ -0,0 +1,388 @@
+-- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
+--
+-- Host: localhost    Database: univesity_system
+-- ------------------------------------------------------
+-- Server version	8.0.45
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admins`
+--
+
+DROP TABLE IF EXISTS `admins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admins` (
+  `admin_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `phone` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`admin_id`),
+  UNIQUE KEY `user_id` (`user_id`),
+  CONSTRAINT `admins_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admins`
+--
+
+LOCK TABLES `admins` WRITE;
+/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
+INSERT INTO `admins` VALUES (2,21,NULL);
+/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `advisors`
+--
+
+DROP TABLE IF EXISTS `advisors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `advisors` (
+  `advisor_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `department` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `phone` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`advisor_id`),
+  UNIQUE KEY `user_id` (`user_id`),
+  CONSTRAINT `advisors_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `advisors`
+--
+
+LOCK TABLES `advisors` WRITE;
+/*!40000 ALTER TABLE `advisors` DISABLE KEYS */;
+INSERT INTO `advisors` VALUES (6,22,'MIS','01234567899','2026-04-28 13:49:13'),(7,25,NULL,NULL,'2026-04-29 06:57:53');
+/*!40000 ALTER TABLE `advisors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `course_instructors`
+--
+
+DROP TABLE IF EXISTS `course_instructors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `course_instructors` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `course_id` int DEFAULT NULL,
+  `advisor_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `course_id` (`course_id`),
+  KEY `advisor_id` (`advisor_id`),
+  CONSTRAINT `course_instructors_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE,
+  CONSTRAINT `course_instructors_ibfk_2` FOREIGN KEY (`advisor_id`) REFERENCES `advisors` (`advisor_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `course_instructors`
+--
+
+LOCK TABLES `course_instructors` WRITE;
+/*!40000 ALTER TABLE `course_instructors` DISABLE KEYS */;
+/*!40000 ALTER TABLE `course_instructors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `course_prerequisites`
+--
+
+DROP TABLE IF EXISTS `course_prerequisites`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `course_prerequisites` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `course_id` int DEFAULT NULL,
+  `prerequisite_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `course_id` (`course_id`),
+  KEY `prerequisite_id` (`prerequisite_id`),
+  CONSTRAINT `course_prerequisites_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE,
+  CONSTRAINT `course_prerequisites_ibfk_2` FOREIGN KEY (`prerequisite_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `course_prerequisites`
+--
+
+LOCK TABLES `course_prerequisites` WRITE;
+/*!40000 ALTER TABLE `course_prerequisites` DISABLE KEYS */;
+INSERT INTO `course_prerequisites` VALUES (1,7,8);
+/*!40000 ALTER TABLE `course_prerequisites` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `courses`
+--
+
+DROP TABLE IF EXISTS `courses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `courses` (
+  `course_id` int NOT NULL AUTO_INCREMENT,
+  `course_name` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `course_code` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `credit_hours` int DEFAULT NULL,
+  `department` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `level` int DEFAULT NULL,
+  `semester` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `year` int DEFAULT NULL,
+  `advisor_id` int DEFAULT NULL,
+  `capacity` int DEFAULT '50',
+  PRIMARY KEY (`course_id`),
+  UNIQUE KEY `course_code` (`course_code`),
+  KEY `fk_course_advisor` (`advisor_id`),
+  CONSTRAINT `fk_course_advisor` FOREIGN KEY (`advisor_id`) REFERENCES `advisors` (`advisor_id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `courses`
+--
+
+LOCK TABLES `courses` WRITE;
+/*!40000 ALTER TABLE `courses` DISABLE KEYS */;
+INSERT INTO `courses` VALUES (7,'AI','CS404',3,'MIS',4,'Fall',2026,7,250),(8,'DSA','CS304',3,'MIS',3,'Spring',2026,6,250);
+/*!40000 ALTER TABLE `courses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `enrollments`
+--
+
+DROP TABLE IF EXISTS `enrollments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `enrollments` (
+  `enrollment_id` int NOT NULL AUTO_INCREMENT,
+  `student_id` int DEFAULT NULL,
+  `course_id` int DEFAULT NULL,
+  `advisor_id` int DEFAULT NULL,
+  `enrollment_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `status` enum('registered','passed','failed') COLLATE utf8mb4_general_ci DEFAULT 'registered',
+  `approval_status` enum('pending','approved','rejected') COLLATE utf8mb4_general_ci DEFAULT 'pending',
+  `grade` varchar(2) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`enrollment_id`),
+  UNIQUE KEY `unique_enrollment` (`student_id`,`course_id`),
+  KEY `student_id` (`student_id`),
+  KEY `course_id` (`course_id`),
+  KEY `advisor_id` (`advisor_id`),
+  KEY `idx_enrollment_student` (`student_id`),
+  KEY `idx_enrollment_course` (`course_id`),
+  CONSTRAINT `enrollments_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`) ON DELETE CASCADE,
+  CONSTRAINT `enrollments_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE,
+  CONSTRAINT `enrollments_ibfk_3` FOREIGN KEY (`advisor_id`) REFERENCES `advisors` (`advisor_id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `enrollments`
+--
+
+LOCK TABLES `enrollments` WRITE;
+/*!40000 ALTER TABLE `enrollments` DISABLE KEYS */;
+INSERT INTO `enrollments` VALUES (11,9,7,7,'2026-04-29 10:29:46','registered','rejected',NULL),(12,9,8,6,'2026-04-29 10:29:50','passed','approved','B+'),(13,11,8,6,'2026-04-29 11:45:49','passed','approved','A'),(14,11,7,7,'2026-04-29 11:49:41','registered','pending',NULL);
+/*!40000 ALTER TABLE `enrollments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `grades`
+--
+
+DROP TABLE IF EXISTS `grades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `grades` (
+  `grade_id` int NOT NULL AUTO_INCREMENT,
+  `student_id` int DEFAULT NULL,
+  `course_id` int DEFAULT NULL,
+  `midterm` decimal(5,2) DEFAULT '0.00',
+  `final` decimal(5,2) DEFAULT '0.00',
+  `total` decimal(5,2) GENERATED ALWAYS AS ((`midterm` + `final`)) STORED,
+  `advisor_id` int DEFAULT NULL,
+  PRIMARY KEY (`grade_id`),
+  UNIQUE KEY `student_id` (`student_id`,`course_id`),
+  KEY `course_id` (`course_id`),
+  KEY `fk_grades_advisor` (`advisor_id`),
+  CONSTRAINT `fk_grades_advisor` FOREIGN KEY (`advisor_id`) REFERENCES `advisors` (`advisor_id`) ON DELETE SET NULL,
+  CONSTRAINT `grades_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`) ON DELETE CASCADE,
+  CONSTRAINT `grades_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `grades`
+--
+
+LOCK TABLES `grades` WRITE;
+/*!40000 ALTER TABLE `grades` DISABLE KEYS */;
+/*!40000 ALTER TABLE `grades` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lectures`
+--
+
+DROP TABLE IF EXISTS `lectures`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lectures` (
+  `lecture_id` int NOT NULL AUTO_INCREMENT,
+  `course_id` int DEFAULT NULL,
+  `advisor_id` int DEFAULT NULL,
+  `lecture_datetime` datetime DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `description` text COLLATE utf8mb4_general_ci,
+  `room` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`lecture_id`),
+  KEY `course_id` (`course_id`),
+  KEY `advisor_id` (`advisor_id`),
+  CONSTRAINT `lectures_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE,
+  CONSTRAINT `lectures_ibfk_2` FOREIGN KEY (`advisor_id`) REFERENCES `advisors` (`advisor_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lectures`
+--
+
+LOCK TABLES `lectures` WRITE;
+/*!40000 ALTER TABLE `lectures` DISABLE KEYS */;
+INSERT INTO `lectures` VALUES (5,8,6,'2026-04-29 10:00:00','2026-04-29 07:36:30','queue','we will cover all tricks on queues','8'),(6,8,6,'2026-04-30 09:30:00','2026-04-29 08:48:56','stack','we will cover the topic of stack','12');
+/*!40000 ALTER TABLE `lectures` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notifications`
+--
+
+DROP TABLE IF EXISTS `notifications`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notifications` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `title` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `message` text COLLATE utf8mb4_general_ci,
+  `is_read` tinyint(1) DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `type` varchar(50) COLLATE utf8mb4_general_ci DEFAULT 'system',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `idx_notifications_user` (`user_id`),
+  CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notifications`
+--
+
+LOCK TABLES `notifications` WRITE;
+/*!40000 ALTER TABLE `notifications` DISABLE KEYS */;
+INSERT INTO `notifications` VALUES (8,23,'Enrollment Approved','Your enrollment request has been approved',0,'2026-04-29 07:31:25','enrollment'),(9,23,'Enrollment Rejected','Your enrollment request was rejected',0,'2026-04-29 07:33:51','enrollment'),(10,23,'New Lecture Available','New lecture posted: queue',0,'2026-04-29 07:36:30','lecture'),(11,26,'Enrollment Approved','Your enrollment request has been approved',0,'2026-04-29 08:46:50','enrollment'),(12,23,'New Lecture Available','New lecture posted: stack',0,'2026-04-29 08:48:56','lecture'),(13,26,'New Lecture Available','New lecture posted: stack',0,'2026-04-29 08:48:56','lecture');
+/*!40000 ALTER TABLE `notifications` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `students` (
+  `student_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `uni_id` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `department` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `level` int DEFAULT NULL,
+  `phone` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `advisor_id` int DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `gpa` decimal(3,2) DEFAULT '0.00',
+  PRIMARY KEY (`student_id`),
+  UNIQUE KEY `user_id` (`user_id`),
+  UNIQUE KEY `uni_id` (`uni_id`),
+  KEY `advisor_id` (`advisor_id`),
+  CONSTRAINT `students_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  CONSTRAINT `students_ibfk_2` FOREIGN KEY (`advisor_id`) REFERENCES `advisors` (`advisor_id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (9,23,'2026123','MIS',4,'01234567899',NULL,'2026-04-28 23:31:17',0.00),(10,24,NULL,NULL,NULL,NULL,NULL,'2026-04-28 23:31:53',0.00),(11,26,'2026789','MIS',4,'01276548589',NULL,'2026-04-29 08:43:21',0.00);
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `email` varchar(150) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `role` enum('student','advisor','admin') COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `approved` tinyint(1) DEFAULT '0',
+  `code` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT '1',
+  `last_login` datetime DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `reset_token` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `reset_expires` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `code` (`code`),
+  KEY `idx_users_email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (21,'Bola Saaed','bola66@gmail.com','$2b$12$l5if7VnNgXCkyw3s819VoukZGIhVnnvr5kWb0bMmHvdmU.fur4UkK','admin','2026-04-28 13:47:26',0,NULL,1,NULL,'2026-04-28 13:47:26',NULL,NULL),(22,'Mina S. Younan','minas321@gmail.com','$2b$12$QW7M8o7pTTodOysshCNXfONT98Dqh4NVl.1zryT2lyG20DnK7ZPTm','advisor','2026-04-28 13:49:13',0,NULL,1,NULL,'2026-04-28 13:49:47',NULL,NULL),(23,'keroles medhat','keromed33@gmail.com','$2b$12$u0NFH04UcyYDDYSR24.0DO4ZVfmA9lNml4Yesw2kK3ZUgw58nNfC6','student','2026-04-28 23:31:17',0,NULL,1,NULL,'2026-04-29 06:58:22',NULL,NULL),(24,'kero emad','keroem44@gmail.com','$2b$12$InWCZhNclm9qN57OGmMQpuFk0l7yNxSfNw1smFlc5rxJvP9EPRgl2','student','2026-04-28 23:31:53',0,NULL,1,NULL,'2026-04-29 06:58:24',NULL,NULL),(25,'Nabil ELDakhly','nabilda33@gmail.com','$2b$12$eIZOex30Yshj4XpfJoyy6uOV/yd1XUWNzaEQWHMv0Hm6ont6rCi0u','advisor','2026-04-29 06:57:53',0,NULL,1,NULL,'2026-04-29 06:58:25',NULL,NULL),(26,'wasem ayman','wasemay44@gmail.com','$2b$12$rUywXz.r8RNJCapvQABmVussZ11FBbqmJyLbKKOLMKhVh.9e9a/bC','student','2026-04-29 08:43:21',0,NULL,1,NULL,'2026-04-29 08:44:03',NULL,NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-05-01  2:28:55
