61afc6ea98d8306d3cfc71bb2256e47770e1b33c
MinaSameh99 <162881373+MinaSameh99@users.noreply.github.com>
2026-05-01 14:19:11 +0300

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.user import User
-from app.models.student import Student
-from app.models.advisor import Advisor
-from app.models.enrollment import Enrollment
-from app.models.course_prerequisite import CoursePrerequisite
--- app/schemas/grade_schema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel , validator
-
-
-class GradeAssign(BaseModel):
-    
-    
-    enrollment_id:int
-
-    grade:str
